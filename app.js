42630ec23bfb93e101b1cdb6bca74385a8aef3bd
Roman Pashkovsky <petrodzher@gmail.com>
2017-10-19 14:45:15 +0300

    Inital commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const PUBLIC_FOLDER = '/public';
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+PUBLIC_FOLDER+'/index.html')
+})
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('connection status', 'Status: Established connection');
+  socket.on('disconnect', function(){
+    io.emit('connection status', 'Status: Connection lost');
+  });
+});
+
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
