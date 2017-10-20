95ffae3384c6f14e04a60d8386e3d920810ecbba
Roman Pashkovsky <petrodzher@gmail.com>
2017-10-20 14:25:29 +0300

    cors added, removed unneeded files

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cors = require('cors');
 
 const PUBLIC_FOLDER = '/public';
+
+// Enable cors
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname+PUBLIC_FOLDER+'/index.html')
